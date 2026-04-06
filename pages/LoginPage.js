0535443ab8451e096d18e56c9d516eb4bc842526
Asib177 <mdrakibulasibredoy@gmail.com>
2026-04-06 12:48:08 +0600

    Changes files path

---
+++ pages/LoginPage.js
@@ -0,0 +1,29 @@
+class LoginPage {
+    constructor(page) {
+        this.page = page;
+
+        this.usernameInput = 'input[name="username"]';
+        this.passwordInput = 'input[name="password"]';
+        this.loginButton = 'button[type="submit"]';
+        this.dashboardText = 'text=Dashboard';
+    }
+
+    async gotoLoginPage() {
+        await this.page.goto('/web/index.php/auth/login');
+    }
+
+    async login(username, password) {
+        await this.page.fill(this.usernameInput, username);
+        await this.page.fill(this.passwordInput, password);
+        await this.page.click(this.loginButton);
+    }
+
+    async isLoginSuccessful() {
+        await this.page.waitForSelector(this.dashboardText);
+        return await this.page.isVisible(this.dashboardText);
+    }
+}
+
+module.exports = { LoginPage };
+
+
